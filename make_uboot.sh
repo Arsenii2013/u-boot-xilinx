4a614a836308bd5917480af16cc521c402f842c9
Arsenii2013 <a.nepein@g.nsu.ru>
2023-08-07 12:57:47 +0700

    Fix make_uboot.sh

--- make_uboot.sh
+++ make_uboot.sh
@@ -1,8 +1,16 @@
+DTB=../dts/system.dtb
+
+if [ ! -f $DTB ]; then
+    echo "$DTB file dont exist"
+    echo "First, generate Device Tree"
+    exit 1 
+fi
+
 export CROSS_COMPILE=arm-none-eabi-
 export ARCH=arm
 export DEVICE_TREE="system"
 
-cp ../dts/system.dtb arch/arm/dts
+cp $DTB arch/arm/dts
 
 make xilinx_zynq_virt_defconfig
 make
