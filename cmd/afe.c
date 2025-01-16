9072bec39920462f80ab5760f930148c283b9b13
Arsenii2013 <a.nepein@g.nsu.ru>
2025-01-16 15:07:59 +0700

    Fix AFE power down after cpu1 start

--- cmd/afe.c
+++ cmd/afe.c
@@ -5,16 +5,18 @@
 #include <time.h>
 
 #define EMIO_PIN      54
-#define AFE_CONF_PIN  EMIO_PIN + 4
+#define AFE_CONF_PIN  EMIO_PIN + 3
 #define nCONFIG_PIN   AFE_CONF_PIN
 #define nSTATUS_PIN   AFE_CONF_PIN + 1
 #define CONF_DONE_PIN AFE_CONF_PIN + 2
 #define INIT_DONE_PIN AFE_CONF_PIN + 3
 #define DCLK_PIN      AFE_CONF_PIN + 4
 #define DATA_PIN      AFE_CONF_PIN + 5
-
-#define MSEL0_PIN AFE_CONF_PIN + 6
-#define MSEL1_PIN AFE_CONF_PIN + 7
+#define MSEL0_PIN     AFE_CONF_PIN + 6
+#define MSEL1_PIN     AFE_CONF_PIN + 7
+#define PRSNT_PIN     AFE_CONF_PIN + 8
+#define PWR_GD_PIN    AFE_CONF_PIN + 9
+#define PWR_ENA_PIN   AFE_CONF_PIN + 10
 
 void delay_us(unsigned long us){
     unsigned long start = get_timer(0);
@@ -31,9 +33,24 @@ int gpio_wait_value_us(unsigned int gpio, int value, unsigned long us){
     return -1;
 }
 
-int start_config(void){
+int setup_pins(void){
 
 	int ret;
+	ret = gpio_request(PRSNT_PIN, "cmd_gpio");
+	if (ret && ret != -EBUSY) {
+		printf("ERROR: requesting pin PRSNT_PIN failed\n");
+		return -1;
+	}
+	ret = gpio_request(PWR_GD_PIN, "cmd_gpio");
+	if (ret && ret != -EBUSY) {
+		printf("ERROR: requesting pin PWR_GD_PIN failed\n");
+		return -1;
+	}
+	ret = gpio_request(PWR_ENA_PIN, "cmd_gpio");
+	if (ret && ret != -EBUSY) {
+		printf("ERROR: requesting pin PWR_ENA_PIN failed\n");
+		return -1;
+	}
 	ret = gpio_request(MSEL0_PIN, "cmd_gpio");
 	if (ret && ret != -EBUSY) {
 		printf("ERROR: requesting pin MSEL0_PIN failed\n");
@@ -74,18 +91,59 @@ int start_config(void){
 		printf("ERROR: requesting pin DATA_PIN failed\n");
 		return -1;
 	}
+    gpio_direction_input(MSEL0_PIN);
+    gpio_direction_input(MSEL1_PIN);
     
-    gpio_direction_output(MSEL0_PIN, 0);
-    gpio_direction_output(MSEL1_PIN, 0);
     gpio_direction_output(DCLK_PIN, 0);
     gpio_direction_output(DATA_PIN, 0);
 
-    gpio_direction_output(nCONFIG_PIN, 0);
+    gpio_direction_input(PRSNT_PIN);  
+    gpio_direction_input(PWR_GD_PIN);  
+    gpio_direction_output(PWR_ENA_PIN, 0);
+
+    gpio_direction_output(nCONFIG_PIN, 1);
     gpio_direction_input(nSTATUS_PIN);  
     gpio_direction_input(CONF_DONE_PIN);  
     gpio_direction_input(INIT_DONE_PIN);  
+    return 0;
+}
+
+int afe_prsnt(void){
+    int prsntVal = gpio_get_value(PRSNT_PIN);
+    printf("INFO: AFE present %d\n", prsntVal);
+    return CMD_RET_SUCCESS;
+}
+
+enum AFEPwrCmd {
+    OFF = 0,
+    ON = 1,
+    GET = 2
+};
+int afe_pwr(enum AFEPwrCmd cmd){
+    if(cmd == GET){
+        int pgVal = gpio_get_value(PWR_GD_PIN);
+        printf("INFO: AFE power good %d\n", pgVal);
+        return CMD_RET_SUCCESS;
+    } else if(cmd == ON) {
+        printf("INFO: AFE power enable 1\n");
+        gpio_set_value(PWR_ENA_PIN, 1);
+        return CMD_RET_SUCCESS;
+    } else if(cmd == OFF) {
+        printf("INFO: AFE power enable 0\n");
+        gpio_set_value(PWR_ENA_PIN, 0);
+        return CMD_RET_SUCCESS;
+    }
+    return CMD_RET_FAILURE;
+}
+
+int start_config(void){
+    gpio_direction_output(MSEL0_PIN, 0);
+    gpio_direction_output(MSEL1_PIN, 0);
+    delay_us(10000);
+
+    gpio_set_value(nCONFIG_PIN, 0);
     delay_us(1);
-    gpio_direction_output(nCONFIG_PIN, 1);
+    gpio_set_value(nCONFIG_PIN, 1);
     if(gpio_get_value(nSTATUS_PIN) != 0)
         return -1;
     if(gpio_wait_value_us(nSTATUS_PIN, 1, 230) == -1)
@@ -97,9 +155,9 @@ int start_config(void){
 }
 
 void send_bit(uint32_t bit){
-    gpio_direction_output(DATA_PIN, bit);
-    gpio_direction_output(DCLK_PIN, 1);
-    gpio_direction_output(DCLK_PIN, 0);
+    gpio_set_value(DATA_PIN, bit);
+    gpio_set_value(DCLK_PIN, 1);
+    gpio_set_value(DCLK_PIN, 0);
 }
 
 void send_word(uint32_t word){
@@ -128,29 +186,15 @@ int end_config(void){
         return -1;
     if(gpio_wait_value_us(INIT_DONE_PIN, 1, 650) == -1)
         return -1;
+    delay_us(10000);
+    gpio_direction_input(MSEL0_PIN);
+    gpio_direction_input(MSEL1_PIN);
     return 0;
 }
 
-int do_afe(struct cmd_tbl *cmdtp, int flag, int argc,
-           char *const argv[]){
-
-    unsigned long addr;
-    unsigned long size;
-    char *endp;
+int afe_load(unsigned long addr, unsigned long size){
     int ret;
     unsigned long uret;
-
-    if (argc != 3)
-        return CMD_RET_USAGE;
-    
-    addr = hextoul(argv[1], &endp);
-    if (*argv[1] == 0 || *endp != 0)
-        return CMD_RET_USAGE;
-    
-    size = hextoul(argv[2], &endp);
-    if (*argv[2] == 0 || *endp != 0)
-        return CMD_RET_USAGE;
-
     ret = start_config();
     if(ret == -1){
 	    printf("ERROR: start\n");
@@ -172,8 +216,54 @@ int do_afe(struct cmd_tbl *cmdtp, int flag, int argc,
     return CMD_RET_SUCCESS;
 }
 
-U_BOOT_CMD(afe, 3,  0,  do_afe,
-       "Load AFE board firmware",
-       "addr len - load AFE bitstream throught Passive Serial interface\n"
+int do_afe(struct cmd_tbl *cmdtp, int flag, int argc,
+           char *const argv[]){
+
+    if (argc < 2)
+        return CMD_RET_USAGE;
+
+    if(setup_pins() == -1)
+        return CMD_RET_FAILURE;
+
+    if(strncmp(argv[1], "prsnt", 5) == 0){
+        return afe_prsnt();
+    } else if(strncmp(argv[1], "pwr", 1) == 0){
+        enum AFEPwrCmd cmd;
+        if(strncmp(argv[2], "on", 2) == 0){
+            cmd = ON;
+        } else if(strncmp(argv[2], "off", 2) == 0){
+            cmd = OFF;
+        } else if(argc == 2 || strncmp(argv[2], "get", 1) == 0){
+            cmd = GET;
+        } else {
+            return CMD_RET_USAGE;
+        }
+        return afe_pwr(cmd);
+    } else if(strncmp(argv[1], "load", 1) == 0){
+        unsigned long addr;
+        unsigned long size;
+        char *endp;
+
+        addr = hextoul(argv[2], &endp);
+        if (*argv[2] == 0 || *endp != 0)
+            return CMD_RET_USAGE;
+        
+        size = hextoul(argv[3], &endp);
+        if (*argv[3] == 0 || *endp != 0)
+            return CMD_RET_USAGE;
+        return afe_load(addr, size);
+    }
+
+    
+
+    return CMD_RET_USAGE;
+}
+
+U_BOOT_CMD(afe, 4,  0,  do_afe,
+       "AFE board operations:",
+       "[operation type] <arguments>\n"
+       "\tprsnt             - get AFE_PRSNT signal\n"
+       "\tpwr  [get|on|off] - get/set/clear AFE_PWR_GD/AFE_PWR_ENA signals\n"
+       "\tload <addr> <len> - load AFE bitstream\n"
 );
  
